42e696a6426be38c0117644a7747e2d7b38e2a34
Muhammad Reihan Nasir <muhammadreihannasir@mail.ugm.ac.id>
2024-11-29 21:02:55 +0700

    Update buku.py

--- buku/buku.py
+++ buku/buku.py
@@ -2,9 +2,15 @@ import database
 from prettytable import PrettyTable
 
 def tambah(nama_buku, genre, pengarang, penerbit):
-    database.c.execute('INSERT INTO buku (nama_buku, genre, pengarang, penerbit) VALUES (?, ?, ?, ?)',
-              (nama_buku, genre, pengarang, penerbit))
-    database.conn.commit()
+    database.c.execute('SELECT * FROM buku WHERE nama_buku = ?', (nama_buku,))
+    existing_data = database.c.fetchone()
+    if existing_data:
+        print(f"Error: Buku dengan nama '{nama_buku}' sudah ada di database.")
+    else:
+        database.c.execute('INSERT INTO buku (nama_buku, genre, pengarang, penerbit) VALUES (?, ?, ?, ?)',
+                (nama_buku, genre, pengarang, penerbit))
+        database.conn.commit()
+        print(f"\nBuku '{nama_buku}' berhasil ditambahkan!\n")
 
 def tampilkan():
     database.c.execute('SELECT * FROM buku')
@@ -33,16 +39,26 @@ def cari(keyword):
             table.add_row(row)
         print(table)
     else:
-        print()
+        print(f"Tidak ditemukan buku dengan kata kunci '{keyword}'")
 
 def ubah(id_buku, nama_buku, genre, pengarang, penerbit):
-    database.c.execute('UPDATE buku SET nama_buku = ?, genre = ?, pengarang = ?, penerbit = ? WHERE id = ?', 
-              (nama_buku, genre, pengarang, penerbit, id_buku))
-    database.conn.commit()
-    if database.c.rowcount > 0:
-        print(f'Buku ID {id_buku} berhasil diubah!')
+    database.c.execute('SELECT id FROM buku WHERE nama_buku = ? AND id != ?', (nama_buku, id_buku))
+    buku_sama = database.c.fetchone()
+    
+    if buku_sama:
+        print(f"Error: Buku dengan nama '{nama_buku}' sudah ada di database.")
     else:
-        print(f'Buku ID {id_buku} tidak ditemukan.')
+        # Lakukan update jika tidak ada konflik nama
+        database.c.execute(
+            'UPDATE buku SET nama_buku = ?, genre = ?, pengarang = ?, penerbit = ? WHERE id = ?', 
+            (nama_buku, genre, pengarang, penerbit, id_buku)
+        )
+        database.conn.commit()
+        
+        if database.c.rowcount > 0:
+            print(f'Buku ID {id_buku} berhasil diubah!')
+        else:
+            print(f'Buku ID {id_buku} tidak ditemukan.')
 
 def hapus(id_buku):
     database.c.execute('DELETE FROM buku WHERE id = ?', (id_buku,))
